d938e811f079b46da771d08688fe30bba69e2185
Scott Clingan <sclingan1@gmail.com>
2023-03-08 13:32:13 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+       
+  We're coming soon
+
+Hello fellow shoppers! We're currently building our new fashion store. 
+Add your email below to stay up-to-date with announcements and our launch deals.
+
+Email Address
+    </div>
+  )
+}
+
+export default App
