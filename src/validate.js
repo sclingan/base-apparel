3c1981e4ad668b066f0e82dcb68d2d51ff5095a5
Scott Clingan <sclingan1@gmail.com>
2023-03-14 14:21:36 -0500

    Added form validation, error messages

--- src/validate.js
+++ src/validate.js
@@ -11,11 +11,19 @@ document.addEventListener('invalid', function(e){
 
 export default function submitData(e) {
     e.preventDefault();
-    const email = document.getElementById('email');
-    console.log(email.innerHTML);
+    const email = document.getElementById('email').value;
+    const regex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
     const errors = Array.from(document.getElementsByClassName('error-hidden'));
-    errors[0].classList.remove('error-hidden');
-    errors[0].classList.add('error-icon');
-    errors[1].classList.remove('error-hidden')
-    errors[1].classList.add('error');
+    var answer = regex.test(email);
+    if(regex.test(email)) {
+        console.log('submitted');
+        window.location.reload();
+      
+    } else {
+        errors[0].classList.remove('error-hidden');
+        errors[0].classList.add('error-icon');
+        errors[1].classList.remove('error-hidden')
+        errors[1].classList.add('error');
+    }
 }
+
